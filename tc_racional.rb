a60666d9435e5c4f4396b9d1e49b8cb6abbec35f
Hector Royo <alu0100608939@ull.edu.es>
2013-10-17 12:05:17 +0100

    Código comentado y algunos errores arreglados

--- tc_racional.rb
+++ tc_racional.rb
@@ -7,6 +7,22 @@ class Test_Fraccion < Test::Unit::TestCase
 
   def test_suma
     assert_equal(10,Fraccion.new(5,2).suma(Fraccion.new(15,2)))
+    assert_equal(Fraccion.new(58,21),Fraccion.new(3,7).suma(Fraccion.new(7,3)))
+  end
+  
+  def test_resta 
+    assert_equal(4, Fraccion.new(30,5).resta(Fraccion.new(4,2)))
+    assert_equal(1/7,Fraccion.new(6,7).resta(Fraccion.new(5,7)))
+  end
+  
+  def test_producto
+    assert_equal(Fraccion.new(81,48),Fraccion.new(9,6).producto(Fraccion.new(9,8)))
+    assert_equal(Fraccion.new(28,27),Fraccion.new(4,3).producto(Fraccion.new(7,9)))
+  end
+  
+  def test_division
+    assert_equal(Fraccion.new(144,50),Fraccion.new(12,5).division(Fraccion.new(10,12)))
+    assert_equal(Fraccion.new(1000,7000),Fraccion.new(500,3500).division(Fraccion.new(2,2)))
   end
   
   
